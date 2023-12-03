c73e908408a7866afa8c27d32ac3296c61354834
Mark Taylor <m.b.taylor@bristol.ac.uk>
2023-12-03 20:08:10 +0000

    success for day 2a in rust

--- rust/advent/src/main.rs
+++ rust/advent/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::env;
+use regex::Regex;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
@@ -11,6 +12,7 @@ fn main() {
     let result = match id {
         "01a" => calc01a(read_lines()),
         "01b" => calc01b(read_lines()),
+        "02a" => calc02a(read_lines()),
         _ => panic!("Unknown ID {}", id),
     };
     println!("{} -> {}", id, result);
@@ -71,4 +73,77 @@ fn calc01b(lines: Vec<String>) -> i32 {
     }
     calc01a(lines2)
 }
+
+struct Bag(i32, i32, i32);
+
+struct Sample(i32, i32, i32);
+
+struct Game {
+    id: i32,
+    samples: Vec<Sample>,
+}
+
+impl Bag {
+    fn is_possible(&self, game: &Game) -> bool {
+        for sample in &game.samples {
+            if sample.0 > self.0 ||
+               sample.1 > self.1 ||
+               sample.2 > self.2 {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+fn calc02a(lines: Vec<String>) -> i32 {
+    let bag = Bag(12, 13, 14);
+    let mut tot = 0;
+    for game in read_games(lines) {
+        if bag.is_possible(&game) {
+            tot += game.id;
+        }
+    }
+    tot
+}
+
+fn read_games(lines: Vec<String>) -> Vec<Game> {
+    let g_re = Regex::new("Game +([0-9]+): +(.*)").unwrap();
+    let s_re = Regex::new("[^;]+").unwrap();
+    let c_re = Regex::new("[^,]+").unwrap();
+    let t_re = Regex::new("([0-9]+) ([a-z]+)").unwrap();
+    let mut games: Vec<Game> = Vec::new();
+    for line in lines {
+        let Some(caps) = g_re.captures(&line[..]) else {
+            panic!("No match {}", line);
+        };
+        let g_id: i32 = caps[1].parse().expect("Game ID not an int");
+        // println!("{}", g_id);
+        let samples_txt = &caps[2];
+        // println!("   {}", samples_txt);
+        let mut samples: Vec<Sample> = Vec::new();
+        for sample_match in s_re.find_iter(samples_txt) {
+            let sample = sample_match.as_str();
+            // println!("   ...{}", sample);
+            let mut r = 0;
+            let mut g = 0;
+            let mut b = 0;
+            for count_match in c_re.find_iter(sample) {
+                let count = count_match.as_str().trim();
+                let item_caps = t_re.captures(count).expect("No count");
+                let item_n: i32 = item_caps[1].parse().expect("Bad num");
+                match &item_caps[2] {
+                    "red" => r = item_n,
+                    "green" => g = item_n,
+                    "blue" => b = item_n,
+                    _ => (),
+                }
+            }
+            samples.push(Sample(r, g, b));
+        }
+        games.push(Game{id: g_id, samples: samples});
+    }
+    games
+}
+
 
