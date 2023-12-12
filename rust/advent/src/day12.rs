3884cbc6ccfb06bb059e9915823df7a1ad062df7
Mark Taylor <m.b.taylor@bristol.ac.uk>
2023-12-12 11:53:15 +0000

    skeleton for day12

--- rust/advent/src/day12.rs
+++ rust/advent/src/day12.rs
@@ -1,4 +1,15 @@
 
+struct Arrangement {
+    istarts: Vec<usize>,
+}
+
+impl Arrangement {
+    // need to implement this.  Should be easy.
+    fn matches(&self, map: &str) -> bool {
+        false
+    }
+}
+
 pub fn ncr(n: i64, r: i64) -> i64 {
     fact(n) / (fact(r) * fact(n-r))
 }
@@ -11,13 +22,26 @@ pub fn fact(k: i64) -> i64 {
     f
 }
 
+// no - the counts arg should be a slice to support recursion.  How?
+// also, it would be better if the output was an iterator.  How?
+fn possible_arrangements(counts: &Vec<usize>, npos: usize) -> Vec<Arrangement> {
+    let arrs = Vec::new();
+    // need to implement this - recursively?
+    arrs
+}
+
 pub fn calc12a(lines: Vec<String>) -> i64 {
+    let mut tot = 0;
     for line in lines {
         let mut split = line.split_whitespace();
         let map = split.next().unwrap();
-        let counts = split.next().unwrap();
-        let counts: Vec<i64> =
-            counts.split(',').map(|x| x.parse().unwrap()).collect();
+        let npos = map.len();
+        let counts: Vec<usize> =
+            split.next().unwrap()
+           .split(',').map(|x| x.parse().unwrap()).collect();
+        let arrs = possible_arrangements(&counts, npos);
+        let c = arrs.iter().filter(|x| x.matches(map)).count() as i64;
+        tot += c;
     }
-    ncr(5,3)
+    tot
 }
