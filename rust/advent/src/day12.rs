9720af66e7d36955a189b52810d7b7e8a7930557
Mark Taylor <m.b.taylor@bristol.ac.uk>
2023-12-13 12:02:59 +0000

    day 12a, completely different to how I thought I'd do it

--- rust/advent/src/day12.rs
+++ rust/advent/src/day12.rs
@@ -1,33 +1,67 @@
 
-struct Arrangement {
-    istarts: Vec<usize>,
-}
-
-impl Arrangement {
-    // need to implement this.  Should be easy.
-    fn matches(&self, map: &str) -> bool {
-        false
+fn can_match(txt: &str, counts: &[usize]) -> bool {
+    let ngrp = counts.len();
+    let mut igrp = 0;
+    let mut hash_count = 0;
+    for c in txt.as_bytes() {
+        match c {
+            b'#' => {
+                hash_count += 1;
+                if igrp >= counts.len() {
+                    return false;
+                }
+            },
+            b'.' => {
+                if hash_count > 0 {
+                    if hash_count != counts[igrp] {
+                        return false;
+                    }
+                    hash_count = 0;
+                    igrp += 1;
+                }
+            },
+            b'?' => {
+                return true;
+            },
+            _ => {
+                panic!();
+            },
+        }
     }
-}
-
-pub fn ncr(n: i64, r: i64) -> i64 {
-    fact(n) / (fact(r) * fact(n-r))
-}
-
-pub fn fact(k: i64) -> i64 {
-    let mut f = 1;
-    for i in 0..k {
-        f *= i+1;
+    if hash_count > 0 {
+        if hash_count != counts[igrp] {
+            return false;
+        }
+        igrp += 1;
     }
-    f
+    igrp == ngrp
 }
 
-// no - the counts arg should be a slice to support recursion.  How?
-// also, it would be better if the output was an iterator.  How?
-fn possible_arrangements(counts: &Vec<usize>, npos: usize) -> Vec<Arrangement> {
-    let arrs = Vec::new();
-    // need to implement this - recursively?
-    arrs
+fn count_possible_matches(txt: &str, counts: &[usize]) -> i64 {
+    match txt.find('?') {
+        None => {
+            if can_match(txt, counts) {
+                1
+            }
+            else {
+                0
+            }
+        },
+        Some(iq) => {
+            if can_match(txt, counts) {
+                let mut c1 = 0;
+                let mut txt1 = String::from(txt);
+                txt1.replace_range(iq..iq+1, ".");
+                c1 += count_possible_matches(&txt1[..], counts);
+                txt1.replace_range(iq..iq+1, "#");
+                c1 += count_possible_matches(&txt1[..], counts);
+                c1
+            }
+            else {
+                0
+            }
+        },
+    }
 }
 
 pub fn calc12a(lines: Vec<String>) -> i64 {
@@ -39,9 +73,8 @@ pub fn calc12a(lines: Vec<String>) -> i64 {
         let counts: Vec<usize> =
             split.next().unwrap()
            .split(',').map(|x| x.parse().unwrap()).collect();
-        let arrs = possible_arrangements(&counts, npos);
-        let c = arrs.iter().filter(|x| x.matches(map)).count() as i64;
-        tot += c;
+        let nmatch = count_possible_matches(&map[..], &counts[..]);
+        tot += nmatch;
     }
     tot
 }
