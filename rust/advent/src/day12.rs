aee050ba98acb1914df61099cf22ce23b9049e17
Mark Taylor <m.b.taylor@bristol.ac.uk>
2023-12-13 17:39:47 +0000

    reimplement day 12 to be more efficient
    Modify strings in place rather than keep creating new ones.
    However, it's not fast enough to solve part B.

--- rust/advent/src/day12.rs
+++ rust/advent/src/day12.rs
@@ -1,4 +1,38 @@
 
+struct Calculator {
+    txt: String,
+    counts: Vec<usize>,
+}
+
+impl Calculator {
+    fn count_possible_matches(&mut self) -> i64 {
+        match self.txt.find('?') {
+            None => {
+                if can_match(&self.txt[..], &self.counts[..]) {
+                    1
+                }
+                else {
+                    0
+                }
+            },
+            Some(iq) => {
+                if can_match(&self.txt[..], &self.counts[..]) {
+                    let mut c1 = 0;
+                    self.txt.replace_range(iq..iq+1, ".");
+                    c1 += self.count_possible_matches();
+                    self.txt.replace_range(iq..iq+1, "#");
+                    c1 += self.count_possible_matches();
+                    self.txt.replace_range(iq..iq+1, "?");
+                    c1
+                }
+                else {
+                    0
+                }
+            },
+        }
+    }
+}
+
 fn can_match(txt: &str, counts: &[usize]) -> bool {
     let ngrp = counts.len();
     let mut igrp = 0;
@@ -37,33 +71,6 @@ fn can_match(txt: &str, counts: &[usize]) -> bool {
     igrp == ngrp
 }
 
-fn count_possible_matches(txt: &str, counts: &[usize]) -> i64 {
-    match txt.find('?') {
-        None => {
-            if can_match(txt, counts) {
-                1
-            }
-            else {
-                0
-            }
-        },
-        Some(iq) => {
-            if can_match(txt, counts) {
-                let mut c1 = 0;
-                let mut txt1 = String::from(txt);
-                txt1.replace_range(iq..iq+1, ".");
-                c1 += count_possible_matches(&txt1[..], counts);
-                txt1.replace_range(iq..iq+1, "#");
-                c1 += count_possible_matches(&txt1[..], counts);
-                c1
-            }
-            else {
-                0
-            }
-        },
-    }
-}
-
 pub fn calc12a(lines: Vec<String>) -> i64 {
     let mut tot = 0;
     for line in lines {
@@ -73,7 +80,8 @@ pub fn calc12a(lines: Vec<String>) -> i64 {
         let counts: Vec<usize> =
             split.next().unwrap()
            .split(',').map(|x| x.parse().unwrap()).collect();
-        let nmatch = count_possible_matches(&map[..], &counts[..]);
+        let nmatch = Calculator{txt: String::from(map), counts: counts.clone()}
+                    .count_possible_matches();
         tot += nmatch;
     }
     tot
