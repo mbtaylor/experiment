b90a98b5238a2bad1bdec9d98cd7f0373b5f5746
Mark Taylor <m.b.taylor@bristol.ac.uk>
2023-12-21 21:45:48 +0000

    rewrote day 8 b in rust, 6x faster than java

--- rust/advent/src/day08.rs
+++ rust/advent/src/day08.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fmt::Display;
 use std::fmt::Formatter;
 use std::fmt::Result;
@@ -5,7 +6,7 @@ use std::iter::Iterator;
 use regex::Regex;
 use std::collections::HashMap;
 
-#[derive(Eq,PartialEq,Hash)]
+#[derive(Clone,Copy,Debug,Eq,PartialEq,Hash)]
 struct Loc ([u8; 3]);
 
 struct Pair(Loc, Loc);
@@ -15,6 +16,16 @@ struct Chart {
     map: HashMap<Loc,Pair>,
 }
 
+#[derive(Copy,Clone,Debug)]
+struct Offset {
+    dest: Loc,
+    nstep: i64,
+}
+
+struct ZoffMap {
+    map: HashMap<Loc,Vec<Option<Offset>>>,
+}
+
 impl Loc {
     fn from(txt: &str) -> Loc {
         let mut triple = [0u8; 3];
@@ -22,6 +33,44 @@ impl Loc {
             triple[i] = *c;
         }
         Loc(triple)
+    }
+}
+
+impl ZoffMap {
+    fn from_chart(chart: &Chart) -> ZoffMap {
+        let mut map: HashMap<Loc,Vec<Option<Offset>>> = HashMap::new();
+        for loc in chart.map.keys() {
+            let mut offs: Vec<Option<Offset>> = Vec::new();
+            for i in 0..chart.dirs.len() {
+                offs.push(None);
+            }
+            map.insert(loc.clone(), offs);
+        }
+        ZoffMap{map}
+    }
+    fn next_zero(&mut self, chart: &Chart, loc: Loc, ipos: usize) -> Offset {
+        let offs = self.map.get_mut(&loc).unwrap();
+        if let None = offs[ipos] {
+            let dirs = chart.dirs.as_bytes();
+            let ndir = dirs.len() as i64;
+            let mut i: i64 = 0;
+            let mut loc = loc;
+            loop {
+                let pair = chart.map.get(&loc).unwrap();
+                let jpos = ((ipos as i64 + i) % ndir) as usize;
+                loc = match dirs[jpos] {
+                    b'L' => pair.0,
+                    b'R' => pair.1,
+                    _ => panic!(),
+                };
+                if loc.0[2] == b'Z' {
+                    offs[ipos] = Some(Offset{dest: loc, nstep: i + 1});
+                    break;
+                }
+                i += 1;
+            }
+        }
+        offs[ipos].unwrap()
     }
 }
 
@@ -59,6 +108,20 @@ fn read_chart(lines: Vec<String>) -> Chart {
     Chart{dirs, map}
 }
 
+fn unique_value(values: &Vec<i64>) -> Option<i64> {
+    let mut iter = values.iter();
+    let v0 = match iter.next() {
+        None => return None,
+        Some(v) => v,
+    };
+    for v in iter {
+        if v != v0 {
+            return None;
+        }
+    }
+    Some(v0.clone())
+}
+
 pub fn calc08a(lines: Vec<String>) -> i64 {
     let chart = read_chart(lines);
     let start = Loc::from("AAA");
@@ -80,28 +143,30 @@ pub fn calc08a(lines: Vec<String>) -> i64 {
 
 pub fn calc08b(lines: Vec<String>) -> i64 {
     let chart = read_chart(lines);
-    let mut locs: Vec<&Loc> =
+    let mut zoff_map = ZoffMap::from_chart(&chart);
+    let ndir = chart.dirs.len() as i64;
+    let starts: Vec<&Loc> =
         chart.map.keys().filter(|k| k.0[2] == b'A').collect();
-    for (i, c_dir) in chart.dirs.chars().cycle().enumerate() {
-        let mut all_z = true;
-        let mut locs1: Vec<&Loc> = Vec::with_capacity(locs.len());
-        for loc in &locs {
-            let pair = &chart.map.get(loc).unwrap();
-            let loc1 = match c_dir {
-                'L' => &pair.0,
-                'R' => &pair.1,
-                _ => panic!(),
-            };
-            locs1.push(loc1);
-            all_z = all_z && loc1.0[2] == b'Z';
-        }
-        if all_z {
-            return (i + 1) as i64;
-        }
-        locs.clear();
-        for loc1 in locs1 {
-            locs.push(loc1);
+    let nstart = starts.len();
+    let mut highs: Vec<i64> = Vec::new();
+    let mut offs: Vec<Offset> = Vec::new();
+    for start in starts {
+        highs.push(0);
+        offs.push(Offset{dest: start.clone(), nstep: 0});
+    }
+    let high_set: HashSet<i64> = HashSet::new();
+    loop {
+        for is in 0..nstart {
+            let off = offs[is];
+            while highs[is] == 0 ||
+                  highs[is] < *highs.iter().max().unwrap() {
+                let ipos = ((highs[is] + off.nstep) % ndir) as usize;
+                offs[is] = zoff_map.next_zero(&chart, off.dest, ipos);
+                highs[is] += offs[is].nstep;
+            }
+            if let Some(value) = unique_value(&highs) {
+                return value;
+            }
         }
     }
-    panic!()
 }
