a07fb98e4b2f17d35b088380e12b0a8194d64601
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-01 17:18:42 +0000

    tidy

--- julia/advent2025/day01.jl
+++ julia/advent2025/day01.jl
@@ -1,12 +1,15 @@
-
 pair_index(i, pair) = parse(Int32, pair[i])
 pair_index1(pair) = pair_index(1, pair)
 pair_index2(pair) = pair_index(2, pair)
 
+function part1(lines)
+   sorted1 = sort(pair_index1.(split.(lines)))
+   sorted2 = sort(pair_index2.(split.(lines)))
+   sum(map(pair -> abs(pair[2]-pair[1]), zip(sorted1, sorted2)))
+end
+
 lines = readlines("../../data/advent2025/day01.txt")
+println(part1(lines))
 
-sorted1 = sort(pair_index1.(split.(lines)))
-sorted2 = sort(pair_index2.(split.(lines)))
 
-println( sum(map(pair -> abs(pair[2]-pair[1]), zip(sorted1, sorted2))) )
 
