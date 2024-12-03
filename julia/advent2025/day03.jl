2ead0aa68bd58617aae3aac2204445a885a406d2
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-03 19:06:31 +0000

    day 3 part 2

--- julia/advent2025/day03.jl
+++ julia/advent2025/day03.jl
@@ -2,15 +2,34 @@
 function mul_sums(line)
    s = 0
    for m in eachmatch(r"mul\(([0-9]+),([0-9]+)\)", line)
-      s += parse(Int32, m.captures[1]) * parse(Int32, m.captures[2])
+      s += parse(Int64, m.captures[1]) * parse(Int64, m.captures[2])
    end
    s
 end
 
-function part1(lines)
-   sum(map(mul_sums, lines))
+function part2(lines)
+   tot = 0
+   enabled = true
+   for line in lines
+      for m in eachmatch(r"mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)", line)
+         if m.match == "do()"
+            enabled = true
+         elseif m.match == "don't()"
+            enabled = false
+         else
+            if enabled
+               s = parse(Int64, m.captures[1]) * parse(Int64, m.captures[2])
+               tot += s
+            end
+         end
+      end
+   end
+   tot
 end
+
+part1(lines) = sum(map(mul_sums, lines))
 
 lines = readlines("../../data/advent2025/day03.txt")
 
 println(part1(lines))
+println(part2(lines))
