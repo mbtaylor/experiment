2588da823763d889e118abcec33afab8d1f2cbde
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-04 11:21:57 +0000

    day 4 part 1

---
+++ julia/advent2024/day04.jl
@@ -0,0 +1,48 @@
+
+struct WordSearch <: AbstractMatrix{Char}
+   lines::Vector{String}
+   pad::Int32
+   w0::Int32
+   h0::Int32
+end
+WordSearch(lines, pad) = WordSearch(lines, pad, length(lines[1]), length(lines))
+
+# Implement Abstract Array interface
+# https://docs.julialang.org/en/v1/manual/interfaces/#man-interface-array
+Base.size(ws::WordSearch) = (ws.w0 + 2 * ws.pad, ws.h0 + 2 * ws.pad)
+Base.getindex(ws::WordSearch, i::Int, j::Int) = begin
+   pi = i - ws.pad
+   pj = j - ws.pad
+   pi > 0 && pi <= ws.w0 && pj > 0 && pj <= ws.h0 ? ws.lines[pj][pi] : ' '
+end
+
+function slices(leng::Int64)
+   dirs = collect((dx,dy) for dx in -1:1, dy in -1:1 if dx!=0 || dy!=0)[1:end]
+   map(dir->collect((i*dir[1],i*dir[2]) for i in 0:leng-1), dirs)
+end
+
+function part1(lines)
+   leng = 4;
+   pad = leng
+   grid = WordSearch(lines, pad)
+   vecs = slices(leng)
+   tot = 0
+   for j = pad+1:size(grid, 2)-pad
+      for i in pad+1:size(grid, 1)-pad
+         for vec in vecs
+            word = String(collect(grid[i+vec[k][1], j+vec[k][2]]
+                                  for k in 1:leng))
+            if word == "XMAS"
+               tot += 1
+            end
+         end
+      end
+   end
+   tot
+end
+
+lines = readlines("../../data/advent2024/day04.txt")
+
+println(part1(lines))
+ 
+
