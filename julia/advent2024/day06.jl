3dca0494d4655c87f313de35f12255d11f8b1bb7
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-06 12:12:18 +0000

    day 6 part 2
    Working, but a bit slow, it took a minute to run.
    Should be able to parallelise at least.

--- julia/advent2024/day06.jl
+++ julia/advent2024/day06.jl
@@ -7,7 +7,6 @@ struct Grid <: AbstractMatrix{Char}
 end
 Grid(lines, pad) = Grid(lines, pad, length(lines[1]), length(lines))
 
-
 # Implement Abstract Array interface
 # https://docs.julialang.org/en/v1/manual/interfaces/#man-interface-array
 Base.size(g::Grid) = (g.w0 + 2 * g.pad, g.h0 + 2 * g.pad)
@@ -20,6 +19,18 @@ positions(g::Grid) = begin
    ((i, j) for i in g.pad+1:size(g, 1)-g.pad,
                j in g.pad+1:size(g, 2)-g.pad)
 end
+
+struct BlockedGrid <: AbstractMatrix{Char}
+   grid::Grid
+   bx::Int32
+   by::Int32
+end
+Base.size(g::BlockedGrid) = Base.size(g.grid)
+Base.getindex(g::BlockedGrid, i::Int, j::Int) = begin
+   i == g.bx && j == g.by ? '#' : Base.getindex(g.grid, i, j)
+end
+positions(g::BlockedGrid) = positions(g.grid)
+
 
 struct Dir <: AbstractChar
    c::Char
@@ -35,6 +46,27 @@ S = Dir('v',  0, +1)
 W = Dir('<', -1,  0)
 
 directions = [N, E, S, W]
+
+function is_loop(grid)
+   pos::CartesianIndex = findfirst(c -> c in directions, grid)
+   dir = directions[findfirst(d -> d == grid[pos], directions)]
+   states::Set{Tuple{CartesianIndex, Dir}} = Set()
+   while true
+      if grid[pos] == ' '
+         return false
+      elseif (pos, dir) in states
+         return true
+      else
+         push!(states, (pos, dir))
+      end
+      pos1 = pos + CartesianIndex(dir.dx, dir.dy)
+      if grid[pos1] == '#'
+         dir = next_direction(dir)
+      else
+         pos = pos1
+      end
+   end
+end
 
 function part1(lines)
    grid = Grid(lines, 1)
@@ -53,7 +85,23 @@ function part1(lines)
    return length(visited)
 end
 
+function part2(lines)
+   grid = Grid(lines, 1)
+   tot = 0
+   for (i, j) in positions(grid)
+      if grid[i, j] == '.'
+         bgrid = BlockedGrid(grid, i, j)
+         if is_loop(bgrid)
+            tot += 1
+         end
+      end
+   end
+   tot
+end
+
 lines = readlines("../../data/advent2024/day06.txt")
 
 println(part1(lines))
+println(part2(lines))
+
 
