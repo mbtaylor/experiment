bc426967f2e540b644014b715d5a9f944c7bc485
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-06 00:48:47 +0000

    works for test data but not real data day5 part2

--- julia/advent2024/day05.jl
+++ julia/advent2024/day05.jl
@@ -39,13 +39,65 @@ function is_ordered(rules::Vector{Rule}, update::Update)
    true
 end
 
+function page_order(rules::Vector{Rule})
+   rules = copy(rules)
+   seq::Vector{Int64} = []
+   # Look for a number that only appears on the right - it's the highest
+   ihi = findfirst(r -> findfirst(q->q.lo==r.hi, rules) == nothing, rules)
+   hi = rules[ihi].hi
+   while length(rules) > 0
+      ilo = findfirst(r -> findfirst(q->q.hi==r.lo, rules) == nothing, rules)
+      lo = rules[ilo].lo
+      push!(seq, lo)
+      filter!(r -> r.lo != lo, rules)
+   end
+   push!(seq, hi)
+   seq
+end
+
+function reorder(update::Update, order::Vector{Int64})
+   out::Vector{Int64} = []
+   for p in order
+      if findfirst(r->r==p, update.pages) != nothing
+         push!(out, p)
+      end
+   end
+   Update(out)
+end
+
 function part1(lines)
    (rules, updates) = read_proto(lines)
    sum(middle(up) for up in updates if is_ordered(rules, up))
 end
 
-lines = readlines("../../data/advent2024/day05.txt")
+function part2(lines)
+   (rules, updates) = read_proto(lines)
+   order = page_order(rules)
+#println("order: ", order)
+#for up in updates
+#  println(reorder(up, order))
+#end
+   sum(middle(reorder(up, order)) for up in updates if !is_ordered(rules, up))
+end
+
+function diag(lines)
+   (rules, updates) = read_proto(lines)
+   for i in 10:99
+     nlo = length(filter(r->r.lo==i, rules))
+     nhi = length(filter(r->r.hi==i, rules))
+     if nlo != 0 || nhi != 0
+        println(i, "\t", nlo, "\t", nhi)
+     end
+   end
+end
+
+lines = readlines("../../data/advent2024/test05.txt")
+
+# diag(lines)
 
 println(part1(lines))
+
+println(part2(lines))
+
 
 
