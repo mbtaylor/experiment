c102af8e83e6a0f8ee8e4d3b0370cb25d8861128
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-08 11:29:36 +0000

    generalise

--- julia/advent2024/day08.jl
+++ julia/advent2024/day08.jl
@@ -6,9 +6,6 @@ struct Grid <: AbstractMatrix{Char}
    h0::Int32
 end
 Grid(lines, pad) = Grid(lines, pad, length(lines[1]), length(lines))
-
-# Implement Abstract Array interface
-# https://docs.julialang.org/en/v1/manual/interfaces/#man-interface-array
 Base.size(g::Grid) = (g.w0 + 2 * g.pad, g.h0 + 2 * g.pad)
 Base.getindex(g::Grid, i::Int, j::Int) = begin
    pi = i - g.pad
@@ -20,21 +17,21 @@ positions(g::Grid) = begin
                j in g.pad+1:size(g, 2)-g.pad)
 end
 
-function antinodes(a::CartesianIndex{2}, b::CartesianIndex{2})
-   (2b-a, 2a-b)
-end
-
-function part1(lines)
-   grid = Grid(lines, 1+max(length(lines[1]), length(lines)))
+function get_freqs(grid::Grid)
    freqs = Set(grid[CartesianIndex(i)] for i in positions(grid))
    delete!(freqs, '.')
+   freqs
+end
+
+function count_antinodes(grid, antinodes_func)
+   freqs = get_freqs(grid)
    anodes::Set{CartesianIndex{2}} = Set()
    for f in freqs
       points = collect(findall(c -> c==f, grid))
       np = length(points)
       for i in 1:np
           for j in 1:i-1
-             for anode in antinodes(points[i], points[j])
+             for anode in antinodes_func(points[i], points[j])
                  if grid[anode] != ' '
                     push!(anodes, anode)
                  end
@@ -43,6 +40,12 @@ function part1(lines)
       end
    end
    length(anodes)
+end
+
+function part1(lines)
+   grid = Grid(lines, 1+max(length(lines[1]), length(lines)))
+   antinoder(a::CartesianIndex{2}, b::CartesianIndex{2}) = (2b-a, 2a-b)
+   count_antinodes(grid, antinoder)
 end
 
 lines = readlines("../../data/advent2024/day08.txt")
