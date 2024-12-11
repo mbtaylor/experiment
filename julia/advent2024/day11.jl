0ed4d175c352a76c8ced6c8ae58eed9c0b9515c6
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-11 22:23:56 +0000

    day 11 part 2

--- julia/advent2024/day11.jl
+++ julia/advent2024/day11.jl
@@ -4,12 +4,6 @@ to_numbers = line -> line |> split .|> s->parse(Int64, s)
 to_labels = line -> line |> split .|> String
 strip_zeros = s -> replace(s, r"^0+([0-9]+)" => s"\1")
 
-mutable struct BlinkSeq
-   counts::Vector{Int64}
-   stones::Vector{String}
-end
-BlinkSeq(stone::String) = BlinkSeq([1], [stone])
-
 function blink(ins::Vector{String})
    outs::Vector{String} = []
    for n in ins
@@ -33,37 +27,46 @@ function blinks(labels::Vector{String}, count::Int64)
    labels
 end
 
-function add_blink(seq::BlinkSeq)
-   seq.stones = blink(seq.stones)
-   push!(seq.counts, length(seq.stones))
+function count_stones(db::Dict{Tuple{String,Int64}, Int64},
+                      stone::String, nblink::Int64)
+   key = (stone, nblink)
+   if nblink == 0
+      return 1
+   elseif haskey(db, key)
+      return db[key]
+   else
+      if stone == "0"
+         value = count_stones(db, "1", nblink-1)
+      elseif (length(stone) % 2 == 0)
+         nc = length(stone) ÷ 2
+         value = (count_stones(db, strip_zeros(stone[1:nc]), nblink-1) +
+                  count_stones(db, strip_zeros(stone[nc+1:end]), nblink-1))
+      else
+         value = count_stones(db, repr(parse(Int64, stone)*2024), nblink-1)
+      end
+      db[key] = value
+      return value
+   end
 end
 
-function count_stones(db::Dict{String, BlinkSeq}, stone::String, nblink::Int64)
-   if !haskey(db, stone)
-      db[stone] = BlinkSeq(stone)
-   end
-   seq = get(db, stone, nothing)
-   while length(seq.counts) <= nblink
-      add_blink(seq)
-   end
-   seq.counts[nblink+1]
-end
-
-function part1(line::String, nblink::Int)
+function slow_count(line::String, nblink::Int)
    labels = to_labels(line)
    stones = blinks(labels, nblink)
    length(stones)
 end
 
-function part2(line::String, nblink::Int)
+function fast_count(line::String, nblink::Int)
    labels = to_labels(line)
-   db::Dict{String, BlinkSeq} = Dict()
+   db::Dict{Tuple{String,Int64}, Int64} = Dict()
    sum(map(s->count_stones(db, s, nblink), labels))
 end
+
+part1(line) = fast_count(line, 25)
+part2(line) = fast_count(line, 75)
 
 line = readline("../../data/advent2024/day11.txt")
 labels = to_labels(line)
 
-println(part1(line, 25))
-println(part2(line, 25))
+println(part1(line))
+println(part2(line))
 
