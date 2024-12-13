f07382b08a7f6c3d6ba731fb5766ed2b9b970d8d
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-13 13:24:27 +0000

    day 13 part 2
    Same solution as part 1.  Though it took me a long time because
    I couldn't get the answer right.  Turned out I was still looking
    at the test data not the real input.

--- julia/advent2024/day13.jl
+++ julia/advent2024/day13.jl
@@ -15,7 +15,7 @@ function read_xy(regex2::Regex, txt::String)
       parse(Int64, m.captures[2]))
 end
 
-function to_games(lines)
+function to_games1(lines)
    games::Vector{Game} = Vector()
    for i in 1:4:length(lines)
       A = read_xy(r"Button A: X[+]([0-9]+), Y[+]([0-9]+)", lines[i+0])
@@ -26,20 +26,40 @@ function to_games(lines)
    games
 end
 
+offset2 = 10000000000000
+game2(game) = Game(game.A, game.B, XY(game.P.x+offset2, game.P.y+offset2))
+
+function to_games2(lines)
+   to_games1(lines) .|> game2
+end
+
 function solve(game)
    m = [game.A.x game.B.x; game.A.y game.B.y]
    p = [game.P.x; game.P.y]
    (da, db) = inv(m) * p
+   # I bet I should be using Rational here
    (na, nb) = (da, db) .|> round .|> Int64
    m * [na; nb] == p ? (na, nb) : nothing
 end
 
+function cost(solution)
+   solution[1] * 3 + solution[2] * 1
+end
+
 function part1(lines)
-   games = to_games(lines)
+   games = to_games1(lines)
    solutions = filter(s -> s!=nothing, solve.(games))
-   sum(s -> s[1]*3 + s[2]*1, solutions)
+   sum(cost, solutions)
+end
+
+function part2(lines)
+   games = to_games2(lines)
+   solutions = filter(s -> s!=nothing, solve.(games))
+   sum(cost, solutions)
 end
 
 lines = readlines("../../data/advent2024/day13.txt")
 
 println(part1(lines))
+println(part2(lines))
+
