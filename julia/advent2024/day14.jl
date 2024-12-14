65c12c0e7989c69b9266ee1ff45480eade129035
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-14 23:01:39 +0000

    day 14 part 2. Bit slow

--- julia/advent2024/day14.jl
+++ julia/advent2024/day14.jl
@@ -50,7 +50,6 @@ function count_map(rps::Vector{XY})
 end
 
 function part1(lines)
-   robots = read_robots(lines)
    rps = read_robots(lines) .|> r -> robot_position(r, 100)
    rzs = rps .|> izone
    #println(display(collect(zip(rps,rzs))))
@@ -58,8 +57,54 @@ function part1(lines)
    prod(zcounts)
 end
 
+function plot_robots(rps::Vector{XY})
+   xs = map(p -> p.x, rps)
+   ys = map(p -> p.y, rps)
+   scatter(xs, ys)
+end
+
+function count_full_blocks(xys::Vector{XY})
+   m = zeros(dims[1], dims[2])
+   for xy in xys
+      m[xy.x+1, xy.y+1] += 1
+   end
+   siz = 4
+   nblank = 0
+   nz = 0
+   nf = 0
+   for x in 1:dims[1]-siz
+      for y in 1:dims[2]-siz
+         s = sum(m[i+x,j+y] for i in 1:siz, j in 1:siz)
+         if s == 0
+            nz += 1
+         elseif s == siz * siz
+            nf += 1
+         end
+      end
+   end
+   nf
+end
+
+function part2(lines)
+   robots = read_robots(lines)
+   i = 0
+   while true
+      rps = robots .|> r -> robot_position(r, i)
+      cb = count_full_blocks(rps)
+      if cb > 10
+         return i
+      end
+      i += 1
+   end
+end
+
+function plot_robots_at(robots, t)
+   plot_robots(robots .|> r -> robot_position(r, t))
+end
+
 dims = (101, 103)
 lines = readlines("../../data/advent2024/day14.txt")
-robots = read_robots(lines)
+
 println(part1(lines))
+println(part2(lines))
 
