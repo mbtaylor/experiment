6b0d57bfc98f309da0f8514834d2c9c9d7ce1fd6
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-19 22:52:06 +0000

    day 19 part 2

--- julia/advent2024/day19.jl
+++ julia/advent2024/day19.jl
@@ -1,11 +1,12 @@
 
 function read_onsen(lines)
-   towels = split(lines[1], ", ")
+   towels = split(lines[1], ", ") .|> String
    patterns = lines[3:end]
    (towels, patterns)
 end
 
-function count_combos(towels, pattern)
+function count_combos(towels::Vector{String}, pattern::String,
+                      memos::Dict{String,Int64})
    lp = length(pattern)
    if lp == 0
       return 1
@@ -14,7 +15,11 @@ function count_combos(towels, pattern)
       for t in towels
          lt = length(t)
          if lp >= lt && pattern[1:lt] == t
-            tot += count_combos(towels, pattern[lt+1:end])
+            pat1 = pattern[lt+1:end]
+            if !haskey(memos, pat1)
+                memos[pat1] = count_combos(towels, pat1, memos)
+            end
+            tot += memos[pat1]
          end
       end
       return tot
@@ -30,17 +35,16 @@ end
 function part2(lines)
    (towels, patterns) = read_onsen(lines)
    tot = 0
+   memos::Dict{String,Int64} = Dict()
    for p in patterns
-      c = count_combos(towels, p)
+      c = count_combos(towels, p, memos)
       tot += c
    end
    return tot
 end
 
-lines = readlines("../../data/advent2024/test19.txt")
+lines = readlines("../../data/advent2024/day19.txt")
 (towels, patterns) = read_onsen(lines)
 
 println(part1(lines))
-
-# println(count_combos(towels, patterns[1]))
 println(part2(lines))
