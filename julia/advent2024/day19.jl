8d5de790c514391aaa34741eef5f8f19d000419d
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-12-19 22:36:32 +0000

    day 19 part 2 working but much too slow

--- julia/advent2024/day19.jl
+++ julia/advent2024/day19.jl
@@ -5,13 +5,42 @@ function read_onsen(lines)
    (towels, patterns)
 end
 
+function count_combos(towels, pattern)
+   lp = length(pattern)
+   if lp == 0
+      return 1
+   else
+      tot = 0
+      for t in towels
+         lt = length(t)
+         if lp >= lt && pattern[1:lt] == t
+            tot += count_combos(towels, pattern[lt+1:end])
+         end
+      end
+      return tot
+   end
+end
+
 function part1(lines)
    (towels, patterns) = read_onsen(lines)
    regex = Regex("^(" * join(towels, "|") * raw")+$")
    count(match(regex, p) != nothing for p in patterns)
 end
 
-lines = readlines("../../data/advent2024/day19.txt")
+function part2(lines)
+   (towels, patterns) = read_onsen(lines)
+   tot = 0
+   for p in patterns
+      c = count_combos(towels, p)
+      tot += c
+   end
+   return tot
+end
+
+lines = readlines("../../data/advent2024/test19.txt")
 (towels, patterns) = read_onsen(lines)
 
 println(part1(lines))
+
+# println(count_combos(towels, patterns[1]))
+println(part2(lines))
