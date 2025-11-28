cc1092b6a19288f67d038116c2952860184cecab
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-11-28 14:35:28 +0000

    hello world with heap-resident stdout writer

--- zig/hello/src/main.zig
+++ zig/hello/src/main.zig
@@ -1,27 +1,21 @@
 const std = @import("std");
 const hello = @import("hello");
+const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try hello.bufferedPrint();
+    std.debug.print("This goes to {s}\n", .{"stderr"});
+
+    var gpa = std.heap.DebugAllocator(.{}){};
+    const stdout_alloc = gpa.allocator();
+    const stdout = try create_stdout(stdout_alloc, @as(usize, 1024));
+    try stdout.print("This goes to {s}\n", .{"stdout"});
+    try stdout.flush();
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+fn create_stdout(allocator: Allocator, siz: usize) !*std.Io.Writer {
+    const buf: []u8 = try allocator.alloc(u8, siz);
+    std.debug.print("buf: {any}", .{buf.len});
+    const writer_ptr = try allocator.create(std.fs.File.Writer);
+    writer_ptr.* = std.fs.File.stdout().writer(buf);
+    return &writer_ptr.interface;
 }
