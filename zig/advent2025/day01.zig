3f0186b5fe4bb0f0c99532d0cf4d297865a57834
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-12-01 09:25:05 +0000

    day 1 part 2

--- zig/advent2025/day01.zig
+++ zig/advent2025/day01.zig
@@ -14,6 +14,8 @@ pub fn main() !void {
 
     const p1 = try part1(lines);
     std.debug.print("part 1: {d}\n", .{p1});
+    const p2 = try part2(lines);
+    std.debug.print("part 2: {d}\n", .{p2});
 }
 
 fn part1(lines: [][]const u8) !u32 {
@@ -31,6 +33,28 @@ fn part1(lines: [][]const u8) !u32 {
         if (@mod(p, @as(u32, 100)) == 0) {
             n0 += 1;
         }
+    }
+    return n0;
+}
+
+fn part2(lines: [][]const u8) !u32 {
+    // brute force!
+    var p: i32 = 50;
+    var n0: u32 = 0;
+    for (lines) |line| {
+       const c0: u8 = line[0];
+       const sense: i32 = try switch (c0) {
+           'L' => @as(i2, -1),
+           'R' => @as(i2, 1),
+           else => error.ParseLine,
+       };
+       var num = try std.fmt.parseInt(u32, line[1..], 10);
+       while (num > 0) : (num -= 1) {
+           p += sense;
+           if (@mod(p, @as(u32, 100)) == 0) {
+               n0 += 1;
+           }
+       }
     }
     return n0;
 }
