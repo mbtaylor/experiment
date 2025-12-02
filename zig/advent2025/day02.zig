08912513d5fd5c148520a2adf4637887590c9221
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-12-02 14:36:33 +0100

    compiles, almost does the right thing unless there are bugs

--- zig/advent2025/day02.zig
+++ zig/advent2025/day02.zig
@@ -3,7 +3,8 @@ const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const MAXBUF: usize = 100_000;
 
-const filename = "data/input02.txt";
+// const filename = "data/input02.txt";
+const filename = "test02.txt";
 
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}){};
@@ -14,6 +15,79 @@ pub fn main() !void {
     defer data_lines.deinit();
     const ranges = try readRanges(allocator, data_lines.line_list[0]);
     defer allocator.free(ranges);
+
+    const p1 = try part1(ranges);
+    std.debug.print("Part 1: {d}\n", .{p1});
+}
+
+fn part1(ranges: []const Range) !u64 {
+    var ninv: u64 = 0;
+    for (ranges) |range| {
+        ninv += try countInvalids(range);
+    }
+    return ninv;
+}
+
+fn countInvalids(range: Range) !u64 {
+    const ndlo = range.lotxt.len;
+    const ndhi = range.hitxt.len;
+    if (ndlo == ndhi) {
+        return try countEqualLengthInvalids(range);
+    }
+    else if (ndhi - ndlo > 1) {
+        return error.UhOh;
+    }
+    else if (ndlo % 2 == 0) {
+        const nd2 = ndlo / 2;
+        const factor: u64 = std.math.pow(u64, 10, nd2);
+        const lopref = try std.fmt.parseInt(u64, range.lotxt[0..nd2], 10);
+        const lo: u64 = lopref * factor;
+        const hi: u64 = (lopref + 1) * factor - 1;
+        var buf: [64]u8 = undefined;
+        const rword = try std.fmt.bufPrint(&buf, "{d}-{d}", .{lo, hi});
+        return try countEqualLengthInvalids(try Range.init(rword));
+    }
+    else if (ndhi % 2 == 0) {
+        const nd2 = ndhi / 2;
+        const factor: u64 = std.math.pow(u64, 10, nd2);
+        const hipref = try std.fmt.parseInt(u64, range.hitxt[0..nd2], 10);
+        const lo: u64 = hipref * factor;
+        const hi: u64 = range.hival;
+        var buf: [64]u8 = undefined;
+        const rword = try std.fmt.bufPrint(&buf, "{d}-{d}", .{lo, hi});
+        return try countEqualLengthInvalids(try Range.init(rword));
+    }
+    else {
+        unreachable;
+    }
+}
+
+fn countEqualLengthInvalids(range: Range) !u64 {
+    const ndlo = range.lotxt.len;
+    const ndhi = range.hitxt.len;
+    if (ndlo != ndhi) {
+        return error.UhOh;
+    }
+    if (ndlo % 2 == 0) {
+        const nd2 = ndlo / 2;
+        const factor: u64 = std.math.pow(u64, 10, nd2);
+        const lopref = try std.fmt.parseInt(u64, range.lotxt[0..nd2], 10);
+        const hipref = try std.fmt.parseInt(u64, range.hitxt[0..nd2], 10);
+        var count: u64 = 0;
+        if (range.inRange(lopref*factor + lopref)) {
+            count += 1;
+        }
+        if (hipref > lopref) {
+            if (range.inRange(hipref*factor + hipref)) {
+                count += 1;
+            }
+            count += hipref - lopref - 1;
+        }
+        return count;
+    }
+    else {
+        return 0;
+    }
 }
 
 fn readRanges(allocator: Allocator, line: []const u8) ![]const Range {
@@ -25,7 +99,6 @@ fn readRanges(allocator: Allocator, line: []const u8) ![]const Range {
         ranges[i] = try Range.init(word);
         i += 1;
     }
-//  @assert(i == nr);
     return ranges;
 }
 
@@ -53,6 +126,10 @@ const Range = struct {
             .loval = try std.fmt.parseInt(u64, lotxt, 10),
             .hival = try std.fmt.parseInt(u64, hitxt, 10),
         };
+    }
+
+    pub fn inRange(self: Range, val: u64) bool {
+        return val >= self.loval and val <= self.hival;
     }
 };
 
@@ -83,4 +160,3 @@ const DataLines = struct {
         self.allocator.free(self.buf);
     }
 };
-
