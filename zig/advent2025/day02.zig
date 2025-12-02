08dad5a6e9b4f43b0917c521e963e64cd8e95f9d
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-12-02 08:30:27 +0000

    day 2 started

---
+++ zig/advent2025/day02.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+const MAXBUF: usize = 100_000;
+
+const filename = "data/input02.txt";
+
+pub fn main() !void {
+    var gpa = std.heap.DebugAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.detectLeaks();
+
+    const data_lines = try readLines(allocator, filename);
+    defer data_lines.deinit();
+    const ranges = try readRanges(allocator, data_lines.line_list[0]);
+    defer allocator.free(ranges);
+
+    std.debug.print("line count: {d}\nrange count: {d}\n",
+                    .{data_lines.line_list.len, ranges.len});
+    std.debug.print("range: {s} - {s}\n", .{ranges[0].lotxt, ranges[0].hitxt});
+}
+
+fn readRanges(allocator: Allocator, line: []const u8) ![]const Range {
+    const nr = 1 + std.mem.count(u8, line, ",");
+    const ranges = try allocator.alloc(Range, nr);
+    var splitIt = std.mem.splitScalar(u8, line, ',');
+    var i: usize = 0;
+    while (splitIt.next()) |word| {
+        ranges[i] = Range.init(word);
+        i += 1;
+    }
+//  @assert(i == nr);
+    return ranges;
+}
+
+const Range = struct {
+    word: []const u8,
+    lotxt: []const u8,
+    hitxt: []const u8,
+
+    pub fn init(word: []const u8) Range {
+        var lotxt: []const u8 = undefined;
+        var hitxt: []const u8 = undefined;
+        for (word, 0..) |c, i| {
+            if (c == '-') {
+                lotxt = word[0..i];
+                hitxt = word[i+1..];
+                break;
+            }
+        }
+        return Range{
+            .word = word,
+            .lotxt = lotxt,
+            .hitxt = hitxt,
+        };
+    }
+};
+
+fn readLines(allocator: Allocator, fname: []const u8) !DataLines {
+    const content = try std.fs.cwd().readFileAlloc(allocator, fname, MAXBUF);
+    var line_list: ArrayList([]const u8) = .empty;
+    var splitIt = std.mem.splitScalar(u8, content, '\n');
+    while (splitIt.next()) |line| {
+        try line_list.append(allocator, line);
+    }
+    if (line_list.getLast().len == 0) {
+        _ = line_list.pop();
+    }
+    return DataLines {
+        .allocator = allocator,
+        .line_list = try line_list.toOwnedSlice(allocator),
+        .buf = content,
+    };
+}
+
+const DataLines = struct {
+    allocator: Allocator,
+    line_list: [][]const u8,
+    buf: []const u8,
+
+    pub fn deinit(self: DataLines) void {
+        self.allocator.free(self.line_list);
+        self.allocator.free(self.buf);
+    }
+};
+
