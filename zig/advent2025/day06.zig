22b7c8405cb0071ce20a603efc63b8309685e75c
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-12-06 13:09:48 +0000

    day 6 part 2

--- zig/advent2025/day06.zig
+++ zig/advent2025/day06.zig
@@ -14,9 +14,13 @@ pub fn main() !void {
     const lines = data_lines.line_list;
     const input1 = try Input1.init(allocator, lines);
     defer input1.deinit();
+    const input2 = try Input2.init(allocator, lines);
+    defer input2.deinit();
 
     const p1 = part1(input1);
     std.debug.print("Part 1: {d}\n", .{p1});
+    const p2 = part2(input2);
+    std.debug.print("Part 2: {d}\n", .{p2});
 }
 
 pub fn part1(input: Input1) u64 {
@@ -27,6 +31,28 @@ pub fn part1(input: Input1) u64 {
         for (0..input.ny) |iy| {
             const num = input.num(ix, iy);
             result = op.operate(result, num);
+        }
+        sum += result;
+    }
+    return sum;
+}
+
+pub fn part2(input: Input2) u64 {
+    var sum: u64 = 0;
+    const ny = input.ny;
+    const ic0s = input.ic0s;
+    for (0..input.nx) |ix| {
+        const op = input.ops[ix];
+        var result: u64 = op.identity();
+        for (ic0s[ix]..ic0s[ix+1]-1) |icx| { 
+            var value: u64 = 0;
+            for (0..ny) |iy| {
+                const c = input.lines[iy][icx];
+                if (c >= '0' and c <= '9') {
+                    value = 10 * value + c - '0';
+                }
+            }
+            result = op.operate(result, value);
         }
         sum += result;
     }
@@ -109,6 +135,68 @@ const Input1 = struct {
 
     pub fn num(self: Input1, ix: usize, iy: usize) u32 {
         return self.grid[iy * self.nx + ix];
+    }
+};
+
+const Input2 = struct {
+    allocator: Allocator,
+    lines: [][]const u8,
+    ny: usize,
+    nx: usize,
+    ncx: usize,
+    ic0s: []usize,
+    ops: []Op,
+
+    pub fn init(allocator: Allocator, lines: [][]const u8) !Input2 {
+        const ny = lines.len - 1;
+        const words0 = try readWords(allocator, lines[0]);
+        const nx = words0.len;
+        allocator.free(words0);
+        const ncx = lines[0].len;
+
+        const ops = try allocator.alloc(Op, nx);
+        const op_words = try readWords(allocator, lines[ny]);
+        for (op_words, 0..) |word, ix| {
+            ops[ix] = try switch (word[0]) {
+                '+' => Op.plus,
+                '*' => Op.times,
+                else => error.UnknownOp,
+            };
+        }
+        allocator.free(op_words);
+
+        const ic0s = try allocator.alloc(usize, nx+1);
+        ic0s[0] = 0;
+        ic0s[nx] = ncx + 1;
+        var jx: usize = 1;
+        for (0..ncx) |icx| {
+            var is_delim: bool = true;
+            for (0..ny) |iy| {
+                if (lines[iy][icx] != ' ') {
+                    is_delim = false;
+                    break;
+                }
+            }
+            if (is_delim) {
+                ic0s[jx] = icx + 1;
+                jx += 1;
+            }
+        }
+
+        return Input2{
+            .allocator = allocator,
+            .lines = lines,
+            .ny = ny,
+            .nx = nx,
+            .ncx = ncx,
+            .ic0s = ic0s,
+            .ops = ops,
+        };
+    }
+
+    pub fn deinit(self: Input2) void {
+        self.allocator.free(self.ops);
+        self.allocator.free(self.ic0s);
     }
 };
 
