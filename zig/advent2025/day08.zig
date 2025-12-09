46a062570f057496d10fb5173b905fbb4e520939
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-12-09 01:43:03 +0000

    useless and broken.
    I don't know what's going on with AutoHashMap.

--- zig/advent2025/day08.zig
+++ zig/advent2025/day08.zig
@@ -4,12 +4,14 @@ const MAXBUF: usize = 100_000;
 const MAXDIST: u64 = u64.max;
 var gpa = std.heap.DebugAllocator(.{}){};
 
-// const filename = "data/input08.txt";
+// const filename: []const u8 = "data/input08.txt;
+// const np: usize = 1000;
 const filename = "test08.txt";
+const np: usize = 10;
 
 pub fn main() !void {
     const allocator = gpa.allocator();
-    defer _ = gpa.detectLeaks();
+//  defer _ = gpa.detectLeaks();
 
     const data_lines = try readLines(allocator, filename);
     defer data_lines.deinit();
@@ -18,35 +20,64 @@ pub fn main() !void {
     const vectors = try readVectors(allocator, lines);
     defer allocator.free(vectors);
 
-    const p1 = try part1(allocator, vectors);
+    const p1 = try part1(allocator, vectors, np);
     std.debug.print("Part 1: {d}\n", .{p1});
 }
 
-pub fn part1(allocator: Allocator, vectors: []const Vector) !u64 {
+pub fn part1(allocator: Allocator, vectors: []const Vector, npair: usize) !u64 {
     const nv = vectors.len;
-    var dists: []u64 = try allocator.alloc(u64, nv*nv);
-    defer allocator.free(dists);
+    var pairs: []Pair = try allocator.alloc(Pair, nv * (nv-1));
+    defer allocator.free(pairs);
+    var ip: usize = 0;
     for (0..nv) |iv| {
+        const v1 = vectors[iv];
         for (0..iv) |jv| {
-            const d = vectors[iv].dist2(vectors[jv]);
-            dists[iv*nv+jv] = d;
-            dists[jv*nv+iv] = d;
+            const v2 = vectors[jv];
+            pairs[ip] = .{
+                .iv1 = jv,
+                .iv2 = iv,
+                .dist2 = v1.dist2(v2),
+            };
+            ip += 1;
         }
     }
-    var ixbuf: []usize = allocator.alloc(usize, nv);
-    defer allocator.free(ixbuf);
-    var ng = 0;
-    var min = MAX_DIST;
-    for (0..nv) |iv| {
-        for (0..iv) |jv| {
-            var d = dists[iv*nv + jv];
-            if (d < min) {
-                ng = 0;
-                max = d;
+    std.mem.sort(Pair, pairs, {}, Pair.cmpByDistance);
+
+    var group_list: std.ArrayList(*IntSet) = .empty;
+    for (pairs[0..npair]) |pair| {
+        var added = false;
+        for (group_list.items) |grp| {
+            if (grp.map.contains(pair.iv1)) {
+                try grp.putInt(pair.iv2);
+                added = true;
             }
-        } 
+            if (grp.map.contains(pair.iv2)) {
+                try grp.putInt(pair.iv1);
+                added = true;
+            }
+        }
+        if (!added) {
+            var group = IntSet.init(allocator);
+            try group.putInt(pair.iv1);
+            try group.putInt(pair.iv2);
+            try group_list.append(allocator, &group);
+        }
     }
-  return 0;
+    const groups = try group_list.toOwnedSlice(allocator);
+    defer {
+//      for (groups) |group| {
+//          group.deinit();
+//      }
+        allocator.free(groups);
+    }
+    std.mem.sort(*IntSet, groups, {}, IntSet.cmpBySize);
+  for (groups) |group| {
+    std.debug.print("    {d}\n", .{group.map.count()});
+    group.print();
+  }
+    return groups[0].map.count()
+         * groups[1].map.count()
+         * groups[2].map.count();
 }
 
 pub fn readVectors(allocator: Allocator, lines: [][]const u8) ![]const Vector {
@@ -55,23 +86,34 @@ pub fn readVectors(allocator: Allocator, lines: [][]const u8) ![]const Vector {
     for (lines, 0..) |line, i| {
         var splitIt = std.mem.splitScalar(u8, line, ',');
         vectors[i] = .{
-            .x = try std.fmt.parseInt(u32, splitIt.next().?, 10),
-            .y = try std.fmt.parseInt(u32, splitIt.next().?, 10),
-            .z = try std.fmt.parseInt(u32, splitIt.next().?, 10),
+            .x = try std.fmt.parseInt(i32, splitIt.next().?, 10),
+            .y = try std.fmt.parseInt(i32, splitIt.next().?, 10),
+            .z = try std.fmt.parseInt(i32, splitIt.next().?, 10),
         };
     }
     return vectors;
 }
 
 const Vector = struct {
-    x: u32,
-    y: u32,
-    z: u32,
+    x: i32,
+    y: i32,
+    z: i32,
 
     pub fn dist2(self: Vector, other: Vector) u64 {
-        return self.x + other.x
-             + self.y + other.y
-             + self.z + other.z;
+        const dx = self.x - other.x;
+        const dy = self.y - other.y;
+        const dz = self.z - other.z;
+        return @intCast(dx*dx + dy*dy + dz*dz);
+    }
+};
+
+const Pair = struct {
+    iv1: usize,
+    iv2: usize,
+    dist2: u64,
+
+    fn cmpByDistance(context: void, p1: Pair, p2: Pair) bool {
+        return std.sort.asc(u64)(context, p1.dist2, p2.dist2);
     }
 };
 
@@ -91,12 +133,24 @@ const IntSet: type = struct {
         self.map.deinit();
     }
 
-    pub fn put_int(self: *IntSet, num: u64) !void {
-        try self.map.put(num, {});
+    pub fn putInt(self: *IntSet, num: usize) !void {
+        try self.map.put(@intCast(num), {});
     }
 
     pub fn intIterator(self: IntSet) MapType.KeyIterator {
         return self.map.keyIterator();
+    }
+
+    fn cmpBySize(context: void, s1: *IntSet, s2: *IntSet) bool {
+        return std.sort.desc(usize)(context, s1.map.count(), s2.map.count());
+    }
+
+    pub fn print(self: IntSet) void {
+        var it = self.map.keyIterator();
+        while (it.next()) |key| {
+            std.debug.print(" {}", .{key});
+        }
+        std.debug.print("\n", .{});
     }
 };
 
